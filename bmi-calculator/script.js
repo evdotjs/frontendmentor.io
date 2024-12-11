6d732d9e728d02505b9a83cbbfecc35cb34e7c26
ev mcneal <evmcneal@icloud.com>
2024-12-10 21:07:25 -0800

    finish styling for mobile version of site

--- bmi-calculator/script.js
+++ bmi-calculator/script.js
@@ -2,6 +2,13 @@ const units = document.getElementsByName('units');
 const [metric, imperial] = units;
 const metricInputs = document.getElementById('metric-inputs');
 const imperialInputs = document.getElementById('imperial-inputs');
+const values = {
+  heightCm: 0,
+  weightKg: 0,
+  heightFt: 0,
+  heightIn: 0,
+  weightLbs: 0,
+};
 
 // display metric units when radio button is clicked
 metric.addEventListener('change', (event) => {
@@ -17,4 +24,32 @@ imperial.addEventListener('change', (event) => {
     imperialInputs.style.display = 'inherit';
     metricInputs.style.display = 'none';
   }
+});
+
+// update values{} whenever user changes a metric value
+metricInputs.addEventListener('change', (event) => {
+  let newValue = parseInt(event.target.value);
+  if (newValue) {
+    if (event.target.name == 'height-cm') {
+      values.heightCm = newValue;
+    } else {
+      values.weightKg = newValue;
+    }
+  }
+  console.log(values);
+});
+
+// update values{} whenever user changes an imperial value
+imperialInputs.addEventListener('change', (event) => {
+  let newValue = parseInt(event.target.value);
+  if (newValue) {
+    if (event.target.name == 'height-ft') {
+      values.heightFt = newValue;
+    } else if (event.target.name == 'height-in') {
+      values.heightIn = newValue;
+    } else {
+      values.weightLbs = newValue;
+    }
+  }
+  console.log(values);
 });
